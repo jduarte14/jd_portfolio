5353771c673befc8b1a97236c1ba452098c92cb4
Joaquin Duarte <joaquinduarte1998@gmail.com>
2023-12-01 20:54:50 -0300

    contact form

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -1,32 +1,14 @@
 import styles from './../styles/contact.module.css';
 import { EarthCanvas } from './canvas/earth';
 import { StarsComponent } from './canvas/stars';
-import React from 'react';
+import Form from './form';
+
+
 const Contact = () => {
     return (
         <div style={{position:"relative"}}>
             <section className={styles.section} >
-                <div className={styles.container}>
-                    <div className={styles.container_info}>
-                        <p>Get in touch</p>
-                        <h3>Contact</h3>
-                    </div>
-                    <form action="" id="contact">
-                        <label htmlFor="">
-                            Your name
-                        </label>
-                        <input type="text" placeholder="What's your name?" />
-                        <label htmlFor="">
-                            Your email
-                        </label>
-                        <input type="text" placeholder="What'e your email?" />
-                        <label htmlFor="">
-                            Your message
-                        </label>
-                        <input type="placeholder" placeholder="What you want to say?" />
-                        <input type="submit" value="Send" className={styles.btn}/>
-                    </form>
-                </div>
+                <Form/>
                 <EarthCanvas />
             </section>
             <aside className={styles.aside}>
---
+++ src/app/components/form.tsx
@@ -0,0 +1,74 @@
+'use client'
+import React, { useRef, FormEvent } from 'react';
+import styles from './../styles/contact.module.css';
+import emailjs from '@emailjs/browser';
+import keys from './../data/keys';
+
+const Form: React.FC = () => {
+    const form = useRef<HTMLFormElement>(null);
+    const message = useRef<HTMLInputElement>(null);
+    const username = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
+
+    const sendEmail = (e: FormEvent) => {
+        e.preventDefault();
+
+        if (form.current && message.current && username.current && emailRef.current) {
+            emailjs
+                .sendForm(keys.serviceId, keys.templateId, form.current, keys.userId)
+                .then(
+                    (response) => {
+                        if (response.status === 200) {
+                            form.current!.reset();
+                            alert("Emaill sended ill contact you soon!");
+                        } else {
+                            throw new Error('There was an error sending the email');
+                        }
+                    },
+                    (error) => {
+                        console.log(error.text);
+                    }
+                );
+        }
+    };
+
+    return (
+        <>
+            <div className={styles.container}>
+                <div className={styles.container_info}>
+                    <p>Get in touch</p>
+                    <h3>Contact</h3>
+                </div>
+                <form id="contact" onSubmit={sendEmail} ref={form}>
+                    <label htmlFor="username">Your name</label>
+                    <input
+                        type="text"
+                        placeholder="What's your name?"
+                        id="username"
+                        ref={username}
+                        required
+                    />
+                    <label htmlFor="email">Your email</label>
+                    <input
+                        type="text"
+                        placeholder="What's your email?"
+                        id="email"
+                        ref={emailRef}
+                        required
+                    />
+                    <label htmlFor="message">Your message</label>
+                    <input
+                        type="text"
+                        placeholder="What you want to say?"
+                        id="message"
+                        ref={message}
+                        required
+                    />
+                    <input type="submit" value="Send" className={styles.btn} />
+                </form>
+            </div>
+        </>
+    );
+};
+
+export default Form;
